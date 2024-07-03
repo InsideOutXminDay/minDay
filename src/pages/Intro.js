5de17567239f4fbfd1b776285a358309d3a2346b
jhw <hyewon3085@naver.com>
2024-07-03 13:37:51 +0900

    IntroFooter 추가

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,9 +1,12 @@
+import IntroFooter from '../components/IntroFooter';
 import IntroHeader from '../components/IntroHeader';
 
 export default function Intro() {
   return (
     <div className="into">
       <IntroHeader />
+      <main></main>
+      <IntroFooter />
     </div>
   );
 }
