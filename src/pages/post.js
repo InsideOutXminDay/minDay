bd0fa548f9f5296af56cbf0ea7a36ea8667f1c3b
mj <ui2939@gmail.com>
2024-07-03 14:39:20 +0900

    PostPage/add guide-card , button

--- src/pages/post.js
+++ src/pages/post.js
@@ -108,6 +108,11 @@ export default function Post() {
                 {img}
                 <p>test test test test test test </p>
             </div>
+            <div className="guide-card">
+                <h3>일반 커뮤니티</h3>
+                <p>서로의 멘탈 관리에 도움이 될 수 있도록 이야기를 공유해 보세요!</p>
+                <button id="new-post-create">작성</button>
+            </div>
         </div>
     )
 }
