f956d775dd595ca44513537885de1a9e2015562b
mj <ui2939@gmail.com>
2024-07-03 19:44:56 +0900

    new page / title, textarea layout

--- src/pages/new.js
+++ src/pages/new.js
@@ -1,43 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { IoCaretBackOutline } from "react-icons/io5";
 import '../styles/new.css';
 
 
-// function Checkbox({ children, disabled, checked, onChange }) {
-//     return (
-//       <label>
-//         <input
-//           type="checkbox"
-//           disabled={disabled}
-//           checked={checked}
-//           onChange={({ target: { checked } }) => onChange(checked)}
-//         />
-//         {children}
-//       </label>
-//     );
-//   }
 
-
-  export default function New(){
+export default function New() {
     const [anonymity, setAnonymity] = useState(false);
 
-    return(
+    return (
         <div className="new-page">
             <form>
-            <div className="new-bar">
-                <IoCaretBackOutline id="new-back"></IoCaretBackOutline >
-                <div className="button-right">
-                <label className="checkbox-right"><input type="checkbox" />익명</label>
-                
-                <span><input type="submit" value="저장하기"/></span>
-            </div>
-            </div>
-            <div className="new-title-bar"><p>
-                <input type="text" placeholder='TITLE' name="title"/></p>
+                <div className="new-bar">
+                    <IoCaretBackOutline id="new-back"></IoCaretBackOutline >
+                    <div className="button-right">
+                        <label className="checkbox-right">
+                            <input type="checkbox" />익명</label>
+                        <span><input type="submit" value="저장하기" /></span>
+                    </div>
                 </div>
-            </form>
+                <div className="new-title-bar"><p>
+                    <input type="text" placeholder='TITLE' name="title" /></p>
+                </div>
+                <div className="new-textarea">
+                    <p><textarea placeholder='contents' name="body"></textarea></p>
+                </div></form>
         </div>
     )
 }
 
- 
