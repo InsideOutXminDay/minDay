639435977fc68ebb771470e88fcbf74ee5ed6a4b
mj <ui2939@gmail.com>
2024-07-03 16:15:15 +0900

    PostPage/edit layout, create test data

--- src/pages/post.js
+++ src/pages/post.js
@@ -2,116 +2,220 @@ import React from "react";
 import '../styles/post.css';
 
 export default function Post() {
-    let img = null;
+
+    const postdb = [{
+        id_post: 1,
+        id_user: 5,
+        title: "test",
+        body: `test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test
+        test test test test test test`,
+        anonymity: false
+    },
+    {
+        id_post: 2,
+        id_user: 6,
+        title: "test",
+        body: `test test test test test test`,
+        anonymity: false
+    }, {
+        id_post: 3,
+        id_user: 2,
+        title: "test",
+        body: `test test test test test test`,
+        anonymity: false
+    }];
+
+
     return (
         <div className="post-page">
             <div className="post-card">
                 <h2>test</h2>
-                {img}
-                <p>test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test testtest test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                test test test test test test 
-                
-                    
-                </p>
-            </div>
-            <div className="post-card">
-                <h2>test</h2>
-                {img}
-                <p>test test test test test test </p>
-            </div>
-            <div className="post-card">
-                <h2>test</h2>
-                {img}
-                <p>test test test test test test </p>
+                <p> test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test
+                    test test test test test test</p>
             </div>
             <div className="guide-card">
                 <h3>일반 커뮤니티</h3>
                 <p>서로의 멘탈 관리에 도움이 될 수 있도록 이야기를 공유해 보세요!</p>
                 <button id="new-post-create">작성</button>
+            </div>
+
+            <div className="post-card">
+                <h2>test</h2>
+                <p>test test test test test test </p>
+            </div>
+            <div className="post-card">
+                <h2>test</h2>
+                <p>test test test test test test </p>
             </div>
         </div>
     )
