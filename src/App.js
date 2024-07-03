04724f718d14b86761ba5cd117f457ad1cdd03ab
jhw <hyewon3085@naver.com>
2024-07-03 20:55:41 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import './App.css';
 import Intro from './pages/Intro';
 
 function App() {
-  return (
-    <div className="App">
-      <Intro></Intro>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
