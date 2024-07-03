1cb818ef5ab1754e2e59cbe360ddcba7a4392c1b
jjggu97 <jjggu@naver.com>
2024-07-03 16:28:58 +0900

    setting ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import './pages/SettingPage.js';
+import './styles/SettingPage.css';
+import './components/SettingAccount.js';
+import SettingPage from './pages/SettingPage.js';
 
 function App() {
   return (
     <div className="App">
+      <SettingPage />
     </div>
   );
 }
