55ced2d618f58355f7238351e37f9e89dcd32e10
Hyemin Oh <orionrora@naver.com>
2024-07-03 14:38:34 +0900

    Header update 2) Home change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
-import Calendar from "../components/Calendar";
 import Header from "../components/Header";
 
 export default function Home(){
     return(
         <div style={{display:"flex"}}>
             <Header/>
-            <Calendar/>
         </div>
     )
 }
