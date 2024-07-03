31257d6e9c77b761726a60cc6feaa8dcef4cfd33
jhw <hyewon3085@naver.com>
2024-07-03 20:37:33 +0900

    IntroPage css 완료

--- src/components/IntroCheckList.js
+++ src/components/IntroCheckList.js
@@ -1,25 +1,28 @@
 import '../styles/IntroCheckList.css';
 export default function IntroCheckList() {
   return (
-    <article>
-      <div>
+    <article className="intro-checklist">
+      <div className="checklist-title">
         <h1>작지만 아주 기본인 것</h1>
         <span>건강한 멘탈은 곧 건강한 신체!</span>
         <span>작은 것부터 실천해가며 루틴화 시켜보세요.</span>
       </div>
-      <img src="/img/checklist.png" alt="checklist-image"></img>
-      <section>
-        <span>매일 자신의 상태를 체크해 보세요</span>
-        <p>
-          적당한 수면 시간, 적당한 활동량, 균형잡힌 식사를 정해진 시간에 먹는
-          것, 나의 스트레스를 날려버릴 활동!
-        </p>
-        <p>
-          마음이 지치면 이런 사소한 것도 챙기기 어려워요. minDay가 매일
-          데일리체크를 통해 현재 상태를 알려드려요!
-        </p>
-        <p>체크리스트를 하나씩 달성해가며 나를 챙겨보는 게 어떤가요?</p>
-      </section>
+
+      <div className="check-content">
+        <img src="/img/checklist.png" alt="checklist-image"></img>
+        <section>
+          <span>매일 자신의 상태를 체크해 보세요.</span>
+          <p>
+            적당한 수면 시간, 적당한 활동량, 균형잡힌 식사를 정해진 시간에 먹는
+            것, 나의 스트레스를 날려버릴 활동!
+          </p>
+          <p>
+            마음이 지치면 이런 사소한 것도 챙기기 어려워요. minDay가 매일
+            데일리체크를 통해 현재 상태를 알려드려요!
+          </p>
+          <p>체크리스트를 하나씩 달성해가며 나를 챙겨보는 게 어떤가요?</p>
+        </section>
+      </div>
     </article>
   );
 }
--- src/components/IntroContents.js
+++ src/components/IntroContents.js
@@ -1,20 +1,23 @@
 import '../styles/IntroContents.css';
 export default function IntroContents() {
   return (
-    <article>
-      <div>
+    <article className="intro-contents">
+      <div className="contents-title">
         <h1>지금 나에게 필요한 컨텐츠</h1>
-        <span>필요할 때마다 인터넷 검색을 할 필요가 없어요</span>
+        <span>필요할 때마다 인터넷 검색을 할 필요가 없어요.</span>
         <span>minDay에서 바로 즐기세요!</span>
       </div>
-      <img src="/img/contents.png" alt="contents-image"></img>
-      <section>
-        <span>마음에게도 기분전환을 시켜주는 다양한 컨텐츠</span>
-        <p>
-          편한 숙면을, 복잡한 마음을 진정시켜줄 다양한 컨텐츠! minDay의 회원들이
-          직접 체험하고 남긴 후기를 보고 한번 결정해보세요!
-        </p>
-      </section>
+      <div className="contents-content">
+        <img src="/img/contents.png" alt="contents-image"></img>
+        <section>
+          <span>마음에게도 기분전환을 시켜주는</span>
+          <span>다양한 컨텐츠</span>
+          <p>편한 숙면을, 복잡한 마음을 진정시켜줄 다양한 컨텐츠!</p>
+          <p>
+            minDay의 회원들이 직접 체험하고 남긴 후기를 보고 한번 결정해보세요!
+          </p>
+        </section>
+      </div>
     </article>
   );
 }
