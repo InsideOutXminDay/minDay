9fb082ee6167a9e73b16bbdb88e3cc3bd2d80057
Hyemin Oh <orionrora@naver.com>
2024-07-04 13:51:59 +0900

    캘린더에 기록된 일기, 감정 표시

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,32 +1,88 @@
 import '../styles/Calendar.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import moment from 'moment';
+import { IoPencilOutline } from "react-icons/io5";
+
+
 
 export default function CalendarComponent(){
   const [date, setDate] = useState(new Date());
+  const [diaryData, setDiaryData] = useState([]);
+  const [nowDiary, setNowDiary] = useState(null);
+  const [Isview, setView] = useState(true);
+
+
+  useEffect(() => {
+    // Fetch the diary data from the public folder
+    fetch('/datas/diary.json')
+      .then((response) => response.json())
+      .then((data) => setDiaryData(data))
+  }, []);
+
   const onChange = (day) =>{
+    setView(true)
     setDate(day)
+    const dateStr = moment(day).format('YYYY-MM-DD');
+    const matching = diaryData.find((value)=>value.date === dateStr)
+    if(matching){
+      setNowDiary(matching)
+    }else{setNowDiary(null)}
+    
   }
-    return(
-        <div className="container">
-            <div className="Calendar">
+
+
+  const tileContent = ({ date, view }) => {
+    if (view==='month' ){
+    const dateStr = moment(date).format('YYYY-MM-DD');
+    const matching = diaryData.find((value)=>value.date === dateStr);
+     if (matching) { 
+      return (
+        <div>
+          <img style={{width:"30px"}} src={`/emotion${matching.id_emotion}.png`} />
+        </div>
+        
+      );
+    }
+  }
+    return null;
+  };
+  return(
+      <div className="container">
+          <div className="Calendar">
             <Calendar
                 locale="en"
                 onChange={onChange}
                 value={date}
-                prevLabel={null}
-                nextLabel={null}
                 next2Label={null}
                 prev2Label={null}
                 formatDay={(locale, date) => moment(date).format('D')}
-                // tileContent={tileContent}
+                tileContent={tileContent}
                 showNeighboringMonth={false}
+                onViewChange={()=>setView(false)}
             />
-            </div>
-            <p>{moment(date).format("YYYY년 MM월 DD일")}</p>
+          </div>
+          <div className='emotion-section'>
+            {
+              !Isview?<p></p>:(
+                nowDiary?
+              <img style={{width:"60px"}} src={`/emotion${nowDiary.id_emotion}.png`} />
+              :<button><IoPencilOutline size={40} color='#fffafd'/></button>
+            )
+              
+            }
+          </div>
+          <div className='diary-section'>
+            {
+              !Isview?<p></p>:(
+                nowDiary?
+                <p>{nowDiary.contents}</p>
+                :<p>다이어리를 입력하세요.</p>
+            )
+            }
+          </div>
 
 
-        </div>
-    )
+      </div>
+  )
 }
