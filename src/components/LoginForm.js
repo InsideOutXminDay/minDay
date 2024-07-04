2481f6d35cb181c55216de52af5b274adbde287d
jhw <hyewon3085@naver.com>
2024-07-04 10:16:35 +0900

    로그인 폼 내용 구성

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,24 @@
+import { ImBubble } from 'react-icons/im';
+import '../styles/LoginForm.css';
 export default function LoginForm() {
   return (
     <article className="login-form">
-      <h1>로그인 화면</h1>
+      <img src="/img/logo_full.png"></img>
+      <h1>Log In</h1>
+      <form>
+        <label htmlFor="id">ID</label>
+        <input id="id" type="text"></input>
+        <label htmlFor="pw">PW</label>
+        <input id="pw" type="password"></input>
+      </form>
+      <div className="btns">
+        <button>Log in</button>
+        <button>
+          Kakao Login
+          <ImBubble />
+        </button>
+      </div>
+      <span>회원 정보를 잊으셨나요?</span>
     </article>
   );
 }
