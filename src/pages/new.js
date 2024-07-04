d7acd17a844760aadb800a2bdf1416d0fdd7570a
mj <ui2939@gmail.com>
2024-07-04 09:39:55 +0900

    new page / checkbox EDIT mind page, post page

--- src/pages/new.js
+++ src/pages/new.js
@@ -5,8 +5,18 @@ import '../styles/new.css';
 
 
 export default function New() {
-    const [anonymity, setAnonymity] = useState(false);
-
+    const [community, setCommunity] = useState("post");
+    
+    const MyCheckbox = (checked)=>{
+        if (checked) {
+            console.log("checked / mind page");
+            setCommunity("mind");
+        } else {
+            console.log("check false / post page");
+            setCommunity("post")
+        }
+    }
+    
     return (
         <div className="new-page">
             <form>
@@ -14,7 +24,8 @@ export default function New() {
                     <IoCaretBackOutline id="new-back"></IoCaretBackOutline >
                     <div className="button-right">
                         <label className="checkbox-right">
-                            <input type="checkbox" />익명</label>
+                            <input type="checkbox" id="checkboxId" 
+                            onClick={(e)=>{MyCheckbox(e.target.checked);}}/>익명</label>
                         <span><input type="submit" value="저장하기" /></span>
                     </div>
                 </div>
