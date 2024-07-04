f0371f1bc1b265015d574d27adb7e00518fa031d
mj <ui2939@gmail.com>
2024-07-04 10:24:25 +0900

    new page / checkbox edit true, false

--- src/pages/new.js
+++ src/pages/new.js
@@ -6,26 +6,31 @@ import '../styles/new.css';
 
 export default function New() {
     const [community, setCommunity] = useState("post");
-    
-    const MyCheckbox = (checked)=>{
+    const [unCheck, setUnCheck] = useState(null);
+
+    const MyCheckbox = (checked) => {
         if (checked) {
-            console.log("checked / mind page");
             setCommunity("mind");
-        } else {
-            console.log("check false / post page");
+            setUnCheck(null);
+        } else if (!checked){
             setCommunity("post")
+            console.log("!checked");
+            setUnCheck(<input type="hidden" name="anonymity" value="post"/>);
         }
     }
-    
+
     return (
         <div className="new-page">
-            <form>
+            <form name="newCreate">
                 <div className="new-bar">
                     <IoCaretBackOutline id="new-back"></IoCaretBackOutline >
-                    <div className="button-right">
-                        <label className="checkbox-right">
-                            <input type="checkbox" id="checkboxId" 
-                            onClick={(e)=>{MyCheckbox(e.target.checked);}}/>익명</label>
+                        <div className="button-right">
+                            <label className="checkbox-right">
+                                <input type="checkbox" id="checkboxId" value={community}
+                                  onClick={(e) => { 
+                                    MyCheckbox(e.target.checked); }} name="anonymity" />익명
+                            </label>
+                            {unCheck}
                         <span><input type="submit" value="저장하기" /></span>
                     </div>
                 </div>
@@ -34,7 +39,8 @@ export default function New() {
                 </div>
                 <div className="new-textarea">
                     <p><textarea placeholder='contents' name="body"></textarea></p>
-                </div></form>
+                </div>
+            </form>
         </div>
     )
 }
