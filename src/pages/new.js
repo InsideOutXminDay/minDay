6fec36cac3a546b35c738cc536e0e24fedfb13ce
mj <ui2939@gmail.com>
2024-07-08 11:12:40 +0900

    new page / post create layout

--- src/pages/new.js
+++ src/pages/new.js
@@ -8,6 +8,10 @@ import '../styles/new.css';
 export default function New() {
     const [community, setCommunity] = useState("post");
     const [unCheck, setUnCheck] = useState(<input type="hidden" name="anonymity" value="post"/>);
+    const location = useLocation();
+    const postInfo = { ...location.state };
+    let backButton = postInfo.lastPage;
+
 
     const MyCheckbox = (checked) => {
         if (checked) {
@@ -19,12 +23,12 @@ export default function New() {
         }
     }
 
-    const location = useLocation();
-    const postInfo = { ...location.state };
-    let backButton = postInfo.lastPage;
-console.log(postInfo.lastPage)
-
-    const newSave = (item)=>{console.log("저장되었습니다 : "+item.title)}
+    const newSave = (item)=>{
+        //db 에 저장되는 것 구현 필요
+        console.log(
+        `저장되었습니다 title : ${item.title}
+        body : ${item.body} 익명 : ${item.anonymity}
+        `)}
 
     return (
         <div className="new-page">
@@ -37,15 +41,7 @@ console.log(postInfo.lastPage)
                 body : e.target.body.value,
                 anonymity : e.target.anonymity.value
             }
-           
-            
-            newSave(item)
-                // 출력 확인용
-                // console.log(
-                //     "anonymity(hidden) : " + document.forms[0].anonymity.value,
-                //     "title : "+document.forms[0].title.value,
-                //     "body : "+document.forms[0].body.value);
-            }}>
+            newSave(item) }}>
                 <div className="new-bar">
                     <NavLink to={backButton}><IoCaretBackOutline id="post-back"></IoCaretBackOutline></NavLink>
                         <div className="button-right">
