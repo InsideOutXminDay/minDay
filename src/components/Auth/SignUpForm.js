f92a23a853cef85420244961eec2153c34cf3b9c
jhw <hyewon3085@naver.com>
2024-07-16 11:49:51 +0900

    fix/비밀번호 submit 관련 분기 2개로 나눠 수정

--- src/components/Auth/SignUpForm.js
+++ src/components/Auth/SignUpForm.js
@@ -5,27 +5,15 @@ import axios from 'axios';
 export default function SignUpForm() {
   const navigate = useNavigate();
 
-  //현재 가입되어 있는 회원 정보(확인용)
-  const [userInfo, setUserInfo] = useState([]);
-
   //회원가입 입력폼 관리
   const [inputForm, setInputForm] = useState({});
-
-  const getData = async () => {
-    const response = await axios.get('http://localhost:4000/api/users');
-    setUserInfo(response.data);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const [submit, setSubmit] = useState(false);
 
   const handleInputForm = (e) => {
     setInputForm({ ...inputForm, [e.target.name]: e.target.value });
   };
 
   const handleCheckDuplicate = async (e) => {
-    console.log('아이디 중복확인');
     const { userId } = inputForm;
     const response = await axios.post('http://localhost:4000/api/checkid', {
       userId,
@@ -43,97 +31,95 @@ export default function SignUpForm() {
 
     if (inputForm.pw !== inputForm.pwCheck) {
       alert('비밀번호를 다시 확인해주세요');
-    }
-    //회원가입 정상적으로 되면 현재 가입된 사용자 정보 배열에 추가(확인용)
-    setUserInfo([
-      ...userInfo,
-      {
+      setInputForm({ ...inputForm, pw: '', pwCheck: '' });
+    } else {
+      setSubmit(true);
+      setInputForm('');
+      await axios.post('http://localhost:4000/api/signup', {
         userId: inputForm.userId,
         pw: inputForm.pw,
         nickname: inputForm.nickname,
         email: inputForm.email,
-      },
-    ]);
-
-    //POST 요청 보내기(서버 메모리에 저장)
-    await axios.post('http://localhost:4000/api/signup', {
-      userId: inputForm.userId,
-      pw: inputForm.pw,
-      nickname: inputForm.nickname,
-      email: inputForm.email,
-    });
-
-    setInputForm('');
-    //회원가입완료되면 로그인 페이지로
-    navigate('/login');
+      });
+    }
   };
 
   return (
     <>
       <div className="signup">
         <img src="/img/logo_full.png"></img>
-        <form className="signup-form" onSubmit={handleSubmit}>
-          <div className="id-box">
-            <label htmlFor="id">아이디</label>
-            <input
-              id="id"
-              type="text"
-              name="userId"
-              value={inputForm.userId || ''}
-              onChange={handleInputForm}
-              autoFocus
-              required
-            ></input>
-            <button className="check-btn" onClick={handleCheckDuplicate}>
-              중복확인
-            </button>
+        {submit ? (
+          <div className="signup-ok">
+            <span className="signup-ok-text">회원가입이 완료되었습니다.</span>
+            <span className="signup-ok-text">
+              바로 로그인해서 minDay를 만들어보세요!
+            </span>
+            <button className="signup-ok-btn">로그인 하러 가기</button>
           </div>
-          <div className="nickname-box">
-            <label htmlFor="nickname">닉네임</label>
-            <input
-              id="nickname"
-              type="text"
-              name="nickname"
-              value={inputForm.nickname || ''}
-              onChange={handleInputForm}
-              required
-            ></input>
-          </div>
-          <div className="email-box">
-            <label htmlFor="email">이메일</label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              value={inputForm.email || ''}
-              onChange={handleInputForm}
-              required
-            ></input>
-          </div>
-          <div className="pw-box">
-            <label htmlFor="pw">비밀번호</label>
-            <input
-              id="pw"
-              type="password"
-              value={inputForm.pw || ''}
-              name="pw"
-              onChange={handleInputForm}
-              required
-            ></input>
-          </div>
-          <div className="pw-sure">
-            <label htmlFor="pw-sure">비밀번호 확인</label>
-            <input
-              id="pw-sure"
-              type="password"
-              name="pwCheck"
-              value={inputForm.pwCheck || ''}
-              onChange={handleInputForm}
-              required
-            ></input>
-          </div>
-          <button className="signup-btn">회원가입</button>
-        </form>
+        ) : (
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <div className="id-box">
+              <label htmlFor="id">아이디</label>
+              <input
+                id="id"
+                type="text"
+                name="userId"
+                value={inputForm.userId || ''}
+                onChange={handleInputForm}
+                autoFocus
+                required
+              ></input>
+              <button className="check-btn" onClick={handleCheckDuplicate}>
+                중복확인
+              </button>
+            </div>
+            <div className="nickname-box">
+              <label htmlFor="nickname">닉네임</label>
+              <input
+                id="nickname"
+                type="text"
+                name="nickname"
+                value={inputForm.nickname || ''}
+                onChange={handleInputForm}
+                required
+              ></input>
+            </div>
+            <div className="email-box">
+              <label htmlFor="email">이메일</label>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={inputForm.email || ''}
+                onChange={handleInputForm}
+                required
+              ></input>
+            </div>
+            <div className="pw-box">
+              <label htmlFor="pw">비밀번호</label>
+              <input
+                id="pw"
+                type="password"
+                value={inputForm.pw || ''}
+                name="pw"
+                onChange={handleInputForm}
+                required
+              ></input>
+            </div>
+            <div className="pw-sure">
+              <label htmlFor="pw-sure">비밀번호 확인</label>
+              <input
+                id="pw-sure"
+                type="password"
+                name="pwCheck"
+                value={inputForm.pwCheck || ''}
+                onChange={handleInputForm}
+                required
+              ></input>
+            </div>
+            <button className="signup-btn">회원가입</button>
+          </form>
+        )}
       </div>
     </>
   );
