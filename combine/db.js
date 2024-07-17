2ce975fac82b391b5f3577eaaac304c956a8a58e
mj <ui2939@gmail.com>
2024-07-17 14:42:53 +0900

    style/db.js handling sensitive data

--- combine/db.js
+++ combine/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const mydb = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234', //로컬 db
-  database: 'insideout', //로컬 db
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, //로컬 db
+  database: process.env.insideout, //로컬 db
+  port: process.env.DB_PORT
 });
 
 mydb.connect((err) => {
