e413c8a7d4e2f1959fdb7916294e1302e0bf06b4
mj <ui2939@gmail.com>
2024-07-17 16:49:06 +0900

    style/ ADD createPool code

--- combine/db.js
+++ combine/db.js
@@ -9,6 +9,14 @@ const mydb = mysql.createConnection({
   port: process.env.REACT_APP_DB_PORT
 });
 
+const pool = mysql.createPool({
+  host: process.env.REACT_APP_DB_HOST,
+  user: process.env.REACT_APP_DB_USER,
+  password: process.env.REACT_APP_DB_PASSWORD, 
+  database: process.env.REACT_APP_DB_DATABASE, 
+  port: process.env.REACT_APP_DB_PORT
+});
+
 mydb.connect((err) => {
   if (err) console.log(err);
   else console.log('Connected to the database');
