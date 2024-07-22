21a55276dbb0350c16eda95c3875f2e7d1d1b465
jhw <hyewon3085@naver.com>
2024-07-22 13:56:12 +0900

    } 빠짐 수정

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ import SignUp from './pages/SignUp.js';
 // diary 데이터
 import Ask from './pages/Ask.js';
 
-
 function App() {
-
   //인증 관련
   const [token, setToken] = useState(localStorage.getItem('token') || '');
 
@@ -50,39 +48,39 @@ function App() {
   };
 
   return (
-        <BrowserRouter>
-          <div className="App">
-            {token && (
-              <li>
-                <button onClick={handleLogout}>Logout</button>
-              </li>
-            )}
-            <Routes>
-              <Route path="/" element={<Intro />} />
-              <Route path="/home" element={<Home />} />{' '}
-              {/* /home/:userid >> 개인별 홈화면 구현 */}
-              {
-                <Route
-                  path="/login"
-                  element={<LoginForm setToken={setAuthToken} />}
-                />
-              }
-              {<Route path="/join" element={<SignUp />} />}
-              <Route path="/ask" element={<Ask />} /> {/* /ask/:id */}
-              {/* <Route path='/find' element={<회원정보 찾기 />}/> */}
-              <Route path="/diary/:id" element={<Diary />} /> {/* /diary/:id */}
-              <Route path="/newdiary" element={<NewDiary />} />{' '}
-              {/* /diary/:id */}
-              <Route path="/contents" element={<Contents />} />
-              <Route path="/setting" element={<SettingPage />} />
-              <Route path="/post" element={<Post />} />
-              <Route path="/mind" element={<Mind />} />
-              <Route path="/new/:id" element={<New />} />
-              <Route path="/edit/:id" element={<Edit />} />
-              <Route path="/detail/:id" element={<Detail />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-);
+    <BrowserRouter>
+      <div className="App">
+        {token && (
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        )}
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/home" element={<Home />} />{' '}
+          {/* /home/:userid >> 개인별 홈화면 구현 */}
+          {
+            <Route
+              path="/login"
+              element={<LoginForm setToken={setAuthToken} />}
+            />
+          }
+          {<Route path="/join" element={<SignUp />} />}
+          <Route path="/ask" element={<Ask />} /> {/* /ask/:id */}
+          {/* <Route path='/find' element={<회원정보 찾기 />}/> */}
+          <Route path="/diary/:id" element={<Diary />} /> {/* /diary/:id */}
+          <Route path="/newdiary" element={<NewDiary />} /> {/* /diary/:id */}
+          <Route path="/contents" element={<Contents />} />
+          <Route path="/setting" element={<SettingPage />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/mind" element={<Mind />} />
+          <Route path="/new/:id" element={<New />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
